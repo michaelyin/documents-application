e46b08bc6ddecd96907da017908f2c65b3d8dbb2
michaelyin <gt8442b@yahoo.com>
2016-05-01 12:07:28 -0400

    1. upgrade to juzu 1.1.0 2. fix the 404 error of Triangle-medium-circle-1-01.png and glyphicons-halflings-white.png

--- src/main/java/org/benjp/documents/portlet/list/package-info.java
+++ src/main/java/org/benjp/documents/portlet/list/package-info.java
@@ -11,22 +11,27 @@
 
 )
 
-@Assets(
-        location = AssetLocation.SERVER,
-        scripts = {
-//                @Script(src = "js/purl.js", depends = "jquery", id="purl"),
-                @Script(src = "js/jquery-juzu-utils-0.1.0.js", id="jquery"),
-                @Script(src = "js/jquery.filedrop.js", depends = "jquery", id = "filedrop"),
-                @Script(src = "js/mustache.js", id="mustache"),
-                @Script(src = "js/taffy-min.js", id="taffy"),
-                @Script(src = "js/main.js", depends = {"jquery", "filedrop", "mustache", "taffy"})
-        },
-        stylesheets = {
-                @Stylesheet(src = "/org/benjp/documents/portlet/list/assets/documents.css", location = AssetLocation.APPLICATION)
-        }
-)
+@Less(@Stylesheet("documents.less"))
+@Assets("*")
 
-@Less(value = "documents.less", minify = true)
+@WebJars(@WebJar("jquery"))
+
+@Scripts({
+	            @Script(id = "jquery", value = "jquery/1.8.3/jquery.js"),
+                @Script(value = "js/jquery-juzu-utils-0.1.0.js", depends = "jquery", id="juzuutil"),
+                @Script(value = "js/jquery.filedrop.js", depends = "jquery", id = "filedrop"),
+                @Script(value = "js/mustache.js", id="mustache"),
+                @Script(value = "js/taffy-min.js", id="taffy"),
+                @Script(value = "js/main.js", depends = {"jquery", "juzuutil", "filedrop", "mustache", "taffy"}, location=AssetLocation.APPLICATION)
+})
+    
+    @Stylesheets ({
+                @Stylesheet(value = "/org/benjp/documents/portlet/list/assets/documents.css", location = AssetLocation.APPLICATION),
+                @Stylesheet(value = "css/bootstrap.min.css", location = AssetLocation.SERVER, id="bootstrap")
+               // @Stylesheet(value = "/org/benjp/documents/portlet/list/assets/documents.css", location = AssetLocation.APPLICATION)
+               // bootstrap.min.css
+        })
+
 
 package org.benjp.documents.portlet.list;
 
@@ -34,10 +39,18 @@ import juzu.Application;
 import juzu.asset.AssetLocation;
 import juzu.plugin.asset.Assets;
 import juzu.plugin.asset.Script;
+import juzu.plugin.asset.Scripts;
 import juzu.plugin.asset.Stylesheet;
+import juzu.plugin.asset.Stylesheets;
 import juzu.plugin.binding.Binding;
 import juzu.plugin.binding.Bindings;
-import juzu.plugin.less.Less;
+import juzu.plugin.less4j.Less;
 import juzu.plugin.portlet.Portlet;
+import juzu.plugin.webjars.WebJar;
+import juzu.plugin.webjars.WebJars;
+
+
+
+
 import org.benjp.provider.GateInMetaProvider;
 
