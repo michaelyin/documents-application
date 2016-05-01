7305e303cddf24f102d07c8bfc3b75714c3b2a40
michaelyin <gt8442b@yahoo.com>
2016-05-01 12:03:35 -0400

    add log.

--- src/main/java/org/benjp/documents/portlet/list/UploadServlet.java
+++ src/main/java/org/benjp/documents/portlet/list/UploadServlet.java
@@ -10,6 +10,8 @@ import org.exoplatform.container.PortalContainer;
 import org.exoplatform.services.jcr.RepositoryService;
 import org.exoplatform.services.jcr.ext.common.SessionProvider;
 import org.exoplatform.services.jcr.ext.hierarchy.NodeHierarchyCreator;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
 
 import javax.jcr.Node;
 import javax.jcr.Session;
@@ -17,229 +19,234 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.List;
 
-public class UploadServlet extends HttpServlet
-{
-  @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-  {
-    doPost(request, response);
-  }
+public class UploadServlet extends HttpServlet {
 
-  @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-  {
+	private static final Log log = ExoLogger.getLogger(UploadServlet.class.getName());
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
 
-    String appContext = request.getHeader("app-context");
-    String path = request.getHeader("app-filter");
-    boolean isPrivateContext = "Personal".equals(appContext);
-    String name = (isPrivateContext)?request.getRemoteUser():request.getHeader("app-space");
-    String uuid = null;
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 
-    try
-    {
-      List<FileItem> items = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
-      for (FileItem item : items)
-      {
-        if (item.isFormField())
-        {
-          String fieldName = item.getFieldName();
-          if ("app-context".equals(fieldName))
-          {
-            appContext = item.getString();
-            isPrivateContext = "Personal".equals(appContext);
-          }
-          if ("app-space".equals(fieldName))
-          {
-            name = (isPrivateContext)?request.getRemoteUser():item.getString();
-          }
-          if ("data-uuid".equals(fieldName)) uuid = item.getString();
-          if ("app-filter".equals(fieldName)) path = item.getString();
+		String appContext = request.getHeader("app-context");
+		String path = request.getHeader("app-filter");
+		boolean isPrivateContext = "Personal".equals(appContext);
+		String name = (isPrivateContext) ? request.getRemoteUser() : request
+				.getHeader("app-space");
+		String uuid = null;
 
-        }
-        if (item.getFieldName().equals("pic"))
-        {
-//          System.out.println("\n#########################");
-//          System.out.println("######### UPLOAD ########");
-//          System.out.println("# name :: "+name);
-//          System.out.println("# isPrivate :: "+isPrivateContext);
-//          System.out.println("# uuid :: "+uuid);
-//          System.out.println("# item value :: "+item.getFieldName());
+		try {
+			List<FileItem> items = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
+			for (FileItem item : items) {
+				if (item.isFormField()) {
+					String fieldName = item.getFieldName();
+					if ("app-context".equals(fieldName)) {
+						appContext = item.getString();
+						isPrivateContext = "Personal".equals(appContext);
+					}
+					if ("app-space".equals(fieldName)) {
+						name = (isPrivateContext) ? request.getRemoteUser()
+								: item.getString();
+					}
+					if ("data-uuid".equals(fieldName))
+						uuid = item.getString();
+					if ("app-filter".equals(fieldName))
+						path = item.getString();
 
-          if (uuid!=null)
-          {
-            storeFile(path, item, name, isPrivateContext, uuid, request);
-            response.setContentType("text/html");
-            response.setCharacterEncoding("UTF-8");
-            response.getWriter().write("<div style='background-color:#ffa; padding:20px'>File has been uploaded successfully!</div>");
-            return;
-          }
-          else
-          {
-            storeFile(path, item, name, isPrivateContext, request);
-            response.setContentType("application/json");
-            response.setCharacterEncoding("UTF-8");
-            response.getWriter().write("{\"status\":\"File has been uploaded successfully!\"}");
-            return;
-          }
-        }
-      }
-    }
-    catch (FileUploadException e)
-    {
-      throw new ServletException("Parsing file upload failed.", e);
-    }
-  }
+				}
+				if (item.getFieldName().equals("pic")) {
+					// System.out.println("\n#########################");
+					// System.out.println("######### UPLOAD ########");
+					// System.out.println("# name :: "+name);
+					// System.out.println("# isPrivate :: "+isPrivateContext);
+					// System.out.println("# uuid :: "+uuid);
+					// System.out.println("# item value :: "+item.getFieldName());
 
-  private void storeFile(String path, FileItem item, String name, boolean isPrivateContext, HttpServletRequest request)
-  {
-    storeFile(path, item, name, isPrivateContext, null, request);
-  }
+					if (uuid != null) {
+						storeFile(path, item, name, isPrivateContext, uuid,
+								request);
+						response.setContentType("text/html");
+						response.setCharacterEncoding("UTF-8");
+						response.getWriter()
+								.write("<div style='background-color:#ffa; padding:20px'>File has been uploaded successfully!</div>");
+						return;
+					} else {
+						storeFile(path, item, name, isPrivateContext, request);
+						response.setContentType("application/json");
+						response.setCharacterEncoding("UTF-8");
+						response.getWriter()
+								.write("{\"status\":\"File has been uploaded successfully!\"}");
+						return;
+					}
+				}
+			}
+		} catch (FileUploadException e) {
+			throw new ServletException("Parsing file upload failed.", e);
+		}
+	}
 
-   private void storeFile(String path, FileItem item, String name, boolean isPrivateContext, String uuid, HttpServletRequest request)
-  {
-    String filename = FilenameUtils.getName(item.getName());
-    RepositoryService repositoryService = (RepositoryService)PortalContainer.getInstance().getComponentInstanceOfType(RepositoryService.class);
-    NodeHierarchyCreator nodeHierarchyCreator = (NodeHierarchyCreator)PortalContainer.getInstance().getComponentInstanceOfType(NodeHierarchyCreator.class);
+	private void storeFile(String path, FileItem item, String name,
+			boolean isPrivateContext, HttpServletRequest request) {
+		storeFile(path, item, name, isPrivateContext, null, request);
+	}
 
+	private void storeFile(String path, FileItem item, String name,
+			boolean isPrivateContext, String uuid, HttpServletRequest request) {
+		String filename = FilenameUtils.getName(item.getName());
+		RepositoryService repositoryService = (RepositoryService) PortalContainer
+				.getInstance().getComponentInstanceOfType(
+						RepositoryService.class);
+		NodeHierarchyCreator nodeHierarchyCreator = (NodeHierarchyCreator) PortalContainer
+				.getInstance().getComponentInstanceOfType(
+						NodeHierarchyCreator.class);
 
-    SessionProvider sessionProvider = SessionProvider.createSystemProvider();
-    try
-    {
-      //get info
-      Session session = sessionProvider.getSession("collaboration", repositoryService.getCurrentRepository());
+		SessionProvider sessionProvider = SessionProvider
+				.createSystemProvider();
+		try {
+			// get info
+			Session session = sessionProvider.getSession("collaboration",
+					repositoryService.getCurrentRepository());
 
-      Node homeNode;
+			Node homeNode;
 
-      if (isPrivateContext)
-      {
-        Node userNode = nodeHierarchyCreator.getUserNode(sessionProvider, name);
-        homeNode = userNode.getNode("Private");
-      }
-      else
-      {
-        Node rootNode = session.getRootNode();
-        homeNode = rootNode.getNode(getSpacePath(name));
-      }
+			if (isPrivateContext) {
+				Node userNode = nodeHierarchyCreator.getUserNode(
+						sessionProvider, name);
+				homeNode = userNode.getNode("Private");
+			} else {
+				Node rootNode = session.getRootNode();
+				homeNode = rootNode.getNode(getSpacePath(name));
+			}
 
-      Node docNode = homeNode.getNode("Documents");
-      if (path.contains("/") && !path.startsWith("Folksonomy/")) {
-        docNode = homeNode.getNode(path);
-      }
+			Node docNode = homeNode.getNode("Documents");
+			if (path.contains("/") && !path.startsWith("Folksonomy/")) {
+				docNode = homeNode.getNode(path);
+			}
 
-//      System.out.println("# docNode :: "+docNode.getPath());
+			// System.out.println("# docNode :: "+docNode.getPath());
 
+			if (!docNode.hasNode(filename)
+					&& (uuid == null || "---".equals(uuid))) {
+				Node fileNode = docNode.addNode(filename, "nt:file");
+				Node jcrContent = fileNode
+						.addNode("jcr:content", "nt:resource");
+				jcrContent.setProperty("jcr:data", item.getInputStream());
+				jcrContent.setProperty("jcr:lastModified",
+						Calendar.getInstance());
+				jcrContent.setProperty("jcr:encoding", "UTF-8");
+				if (filename.endsWith(".jpg"))
+					jcrContent.setProperty("jcr:mimeType", "image/jpeg");
+				else if (filename.endsWith(".png"))
+					jcrContent.setProperty("jcr:mimeType", "image/png");
+				else if (filename.endsWith(".pdf"))
+					jcrContent.setProperty("jcr:mimeType", "application/pdf");
+				else if (filename.endsWith(".doc"))
+					jcrContent.setProperty("jcr:mimeType",
+							"application/vnd.ms-word");
+				else if (filename.endsWith(".xls"))
+					jcrContent.setProperty("jcr:mimeType",
+							"application/vnd.ms-excel");
+				else if (filename.endsWith(".ppt"))
+					jcrContent.setProperty("jcr:mimeType",
+							"application/vnd.ms-powerpoint");
+				else if (filename.endsWith(".docx"))
+					jcrContent
+							.setProperty("jcr:mimeType",
+									"application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+				else if (filename.endsWith(".xlsx"))
+					jcrContent
+							.setProperty("jcr:mimeType",
+									"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+				else if (filename.endsWith(".pptx"))
+					jcrContent
+							.setProperty("jcr:mimeType",
+									"application/vnd.openxmlformats-officedocument.presentationml.presentation");
+				else if (filename.endsWith(".odp"))
+					jcrContent.setProperty("jcr:mimeType",
+							"application/vnd.oasis.opendocument.presentation");
+				else if (filename.endsWith(".odt"))
+					jcrContent.setProperty("jcr:mimeType",
+							"application/vnd.oasis.opendocument.text");
+				else if (filename.endsWith(".ods"))
+					jcrContent.setProperty("jcr:mimeType",
+							"application/vnd.oasis.opendocument.spreadsheet");
+				fileNode.setProperty("exo:lastModifier",
+						request.getRemoteUser());
+				DocumentsData.updateTimestamp(docNode);
+				DocumentsData.updateSize(docNode);
+				DocumentsData.updateTimestamp(docNode.getParent());
+				session.save();
+				uuid = fileNode.getUUID();
+			} else {
+				Node fileNode = null;
+				if (uuid != null) {
+					fileNode = session.getNodeByUUID(uuid);
+				} else {
+					fileNode = docNode.getNode(filename);
+				}
+				if (fileNode.canAddMixin("mix:versionable"))
+					fileNode.addMixin("mix:versionable");
+				if (!fileNode.isCheckedOut()) {
+					fileNode.checkout();
+				}
+				fileNode.save();
+				fileNode.checkin();
+				fileNode.checkout();
+				Node jcrContent = fileNode.getNode("jcr:content");
+				jcrContent.setProperty("jcr:data", item.getInputStream());
+				DocumentsData.updateTimestamp(fileNode);
+				DocumentsData.updateTimestamp(fileNode.getParent());
+				fileNode.setProperty("exo:lastModifier",
+						request.getRemoteUser());
+				fileNode.save();
+				session.save();
+			}
 
-      if (!docNode.hasNode(filename) && (uuid==null || "---".equals(uuid)))
-      {
-        Node fileNode = docNode.addNode(filename, "nt:file");
-        Node jcrContent = fileNode.addNode("jcr:content", "nt:resource");
-        jcrContent.setProperty("jcr:data", item.getInputStream());
-        jcrContent.setProperty("jcr:lastModified", Calendar.getInstance());
-        jcrContent.setProperty("jcr:encoding", "UTF-8");
-        if (filename.endsWith(".jpg"))
-          jcrContent.setProperty("jcr:mimeType", "image/jpeg");
-        else if (filename.endsWith(".png"))
-          jcrContent.setProperty("jcr:mimeType", "image/png");
-        else if (filename.endsWith(".pdf"))
-          jcrContent.setProperty("jcr:mimeType", "application/pdf");
-        else if (filename.endsWith(".doc"))
-          jcrContent.setProperty("jcr:mimeType", "application/vnd.ms-word");
-        else if (filename.endsWith(".xls"))
-          jcrContent.setProperty("jcr:mimeType", "application/vnd.ms-excel");
-        else if (filename.endsWith(".ppt"))
-          jcrContent.setProperty("jcr:mimeType", "application/vnd.ms-powerpoint");
-        else if (filename.endsWith(".docx"))
-          jcrContent.setProperty("jcr:mimeType", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
-        else if (filename.endsWith(".xlsx"))
-          jcrContent.setProperty("jcr:mimeType", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-        else if (filename.endsWith(".pptx"))
-          jcrContent.setProperty("jcr:mimeType", "application/vnd.openxmlformats-officedocument.presentationml.presentation");
-        else if (filename.endsWith(".odp"))
-          jcrContent.setProperty("jcr:mimeType", "application/vnd.oasis.opendocument.presentation");
-        else if (filename.endsWith(".odt"))
-          jcrContent.setProperty("jcr:mimeType", "application/vnd.oasis.opendocument.text");
-        else if (filename.endsWith(".ods"))
-          jcrContent.setProperty("jcr:mimeType", "application/vnd.oasis.opendocument.spreadsheet");
-        fileNode.setProperty("exo:lastModifier", request.getRemoteUser());
-        DocumentsData.updateTimestamp(docNode);
-        DocumentsData.updateSize(docNode);
-        DocumentsData.updateTimestamp(docNode.getParent());
-        session.save();
-        uuid = fileNode.getUUID();
-      }
-      else
-      {
-        Node fileNode=null;
-        if (uuid!=null) {
-          fileNode = session.getNodeByUUID(uuid);
-        }
-        else
-        {
-          fileNode = docNode.getNode(filename);
-        }
-        if (fileNode.canAddMixin("mix:versionable")) fileNode.addMixin("mix:versionable");
-        if (!fileNode.isCheckedOut()) {
-          fileNode.checkout();
-        }
-        fileNode.save();
-        fileNode.checkin();
-        fileNode.checkout();
-        Node jcrContent = fileNode.getNode("jcr:content");
-        jcrContent.setProperty("jcr:data", item.getInputStream());
-        DocumentsData.updateTimestamp(fileNode);
-        DocumentsData.updateTimestamp(fileNode.getParent());
-        fileNode.setProperty("exo:lastModifier", request.getRemoteUser());
-        fileNode.save();
-        session.save();
-      }
+			if (path.startsWith("Folksonomy/")) {
+				if (uuid != null) {
+					if (!isPrivateContext)
+						path = path.replace("Folksonomy/",
+								"ApplicationData/Tags/");
+					// Node fileNode = session.getNodeByUUID(uuid);
+					Node tagNode = homeNode.getNode(path);
+					Node linkNode = tagNode.addNode(filename, "exo:symlink");
+					linkNode.setProperty("exo:uuid", uuid);
+					linkNode.setProperty("exo:workspace", "collaboration");
+					linkNode.setProperty("exo:primaryType", "nt:file");
+					tagNode.save();
+					DocumentsData.updateTimestamp(tagNode);
+					DocumentsData.updateTimestamp(tagNode.getParent());
+					DocumentsData.updateTimestamp(homeNode);
 
-      if (path.startsWith("Folksonomy/")) {
-        if (uuid!=null)
-        {
-          if (!isPrivateContext)
-            path = path.replace("Folksonomy/", "ApplicationData/Tags/");
-          //Node fileNode = session.getNodeByUUID(uuid);
-          Node tagNode = homeNode.getNode(path);
-          Node linkNode = tagNode.addNode(filename, "exo:symlink");
-          linkNode.setProperty("exo:uuid", uuid);
-          linkNode.setProperty("exo:workspace", "collaboration");
-          linkNode.setProperty("exo:primaryType", "nt:file");
-          tagNode.save();
-          DocumentsData.updateTimestamp(tagNode);
-          DocumentsData.updateTimestamp(tagNode.getParent());
-          DocumentsData.updateTimestamp(homeNode);
+					session.save();
 
-          session.save();
+				}
+			}
 
-        }
-      }
+		} catch (Exception e) {
+			log.error("upload error", e);
+		} finally {
+			sessionProvider.close();
+		}
+	}
 
-    }
-    catch (Exception e)
-    {
-      System.out.println("JCR::" + e.getMessage());
-//      e.printStackTrace();
-    }
-    finally
-    {
-      sessionProvider.close();
-    }
-  }
+	private boolean isImage(String filename) {
+		if (filename.endsWith(".jpg") || filename.endsWith(".png"))
+			return true;
+		return false;
+	}
 
-  private boolean isImage(String filename)
-  {
-    if (filename.endsWith(".jpg") || filename.endsWith(".png"))
-      return true;
-    return false;
-  }
-
-  private static String getSpacePath(String space)
-  {
-    return "Groups/spaces/"+space;
-  }
+	private static String getSpacePath(String space) {
+		return "Groups/spaces/" + space;
+	}
 
 }
